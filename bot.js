b0370cd10e15110fa5ff00f4db35baccc6f22039
Klienutt <39462929+Klienutt@users.noreply.github.com>
2018-05-20 22:26:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ logger.add(logger.transports.Console, {
 logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: BOT_TOKEN,
+   token: auth.token,
    autorun: true
 });
 bot.on('ready', function (evt) {
